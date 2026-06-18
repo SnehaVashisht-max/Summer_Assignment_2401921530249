ea77e6f69bd84ee8012a79eb8c04c424a82ab5c2
SnehaVashisht-max <snehavashisht18@gmail.com>
2026-06-18 20:38:01 +0530

    Week_3 Day_4 solution

---
+++ Week_3/Day_4/Largest_Rectangle_in_Histogram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int maxArea = 0;
+        for(int i = 0; i <= n; i++) {
+            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int a = i;
+                int b = st.empty() ? -1 : st.top();
+                int width = a - b - 1;
+                maxArea = max(maxArea, height * width);
+            }
+            st.push(i);
+        }
+    return maxArea; 
+    }
+};
